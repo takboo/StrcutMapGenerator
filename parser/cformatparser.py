c1803cb3fa0e26822f2e3b9f9bfca2e1e8fcd209
wujunhao <wujunhao@kingsoft.com>
2016-09-20 19:42:23 +0800

    git init

---
+++ parser/cformatparser.py
@@ -0,0 +1,7 @@
+# !/usr/bin/python
+# _*_ coding:utf-8 _*_
+from parser.structparser import StructParser
+
+
+class CformatParser(StructParser):
+    pass
---
+++ parser/structparser.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python
+# _*_ coding:utf-8 _*_
+
+
+class StructParser:
+    def __init__(self, data):
+        pass
+
+
+
+
